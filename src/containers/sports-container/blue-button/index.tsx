1aba62e5d0fd3c8727c21a1d7ca901581d3f4f70
unknown <xlongclaw@gmail.com>
2024-04-01 15:24:10 +0530

    refactor(docs): add comments and organise code

--- src/containers/sports-container/blue-button/index.tsx
+++ src/containers/sports-container/blue-button/index.tsx
@@ -1,22 +1,35 @@
-import React from "react";
-import { motion } from "framer-motion";
-import { sportsWrapperVariants } from "../animations";
+import React from 'react';
+import { motion } from 'framer-motion'; // Importing motion from framer-motion for animations
+import { sportsWrapperVariants } from '../animations'; // Importing animation variants
 
+/**
+ * Props interface for the BlueButton component.
+ */
 interface PropsType {
-  title: string;
-  expandedTitle: string;
-  onClick:()=>void;
-  expanded:boolean
+  title: string; // The title text for the button when not expanded.
+  expandedTitle: string; // The title text for the button when expanded.
+  onClick: () => void; // Function to be called when the button is clicked.
+  expanded: boolean; // Flag indicating whether the button is expanded or not.
 }
 
-export default function BlueButton({ title,onClick,expanded,expandedTitle }: PropsType) {
+/**
+ * BlueButton component represents a blue button.
+ * @param title The title text for the button when not expanded.
+ * @param expandedTitle The title text for the button when expanded.
+ * @param onClick Function to be called when the button is clicked.
+ * @param expanded Flag indicating whether the button is expanded or not.
+ * @returns JSX element representing the BlueButton component.
+ */
+export default function BlueButton({ title, onClick, expanded, expandedTitle }: PropsType): React.JSX.Element {
+  // Render the button with motion animation
   return (
     <motion.button
-    variants={sportsWrapperVariants}
-     onClick={onClick} className="bg-[#2C9CF0] hover:bg-[#2c77f0] text-white px-5 py-2 text-[14px] rounded">
-      {
-        !expanded ? title :expandedTitle
-      }
+      variants={sportsWrapperVariants} 
+      onClick={onClick}
+      className="bg-[#2C9CF0] hover:bg-[#2c77f0] text-white px-5 py-2 text-[14px] rounded"
+    >
+      {/* Render the title text or expanded title text based on the expanded flag */}
+      {!expanded ? title : expandedTitle}
     </motion.button>
   );
 }
