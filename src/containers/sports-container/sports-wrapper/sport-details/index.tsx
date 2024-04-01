978366ece814473cac5c4c2cdac06ea96cd80472
unknown <xlongclaw@gmail.com>
2024-04-01 15:17:41 +0530

    refactor: add comments and clean up

--- src/containers/sports-container/sports-wrapper/sport-details/index.tsx
+++ src/containers/sports-container/sports-wrapper/sport-details/index.tsx
@@ -1,23 +1,35 @@
-import React from 'react'
+import React from 'react';
 
+/**
+ * Props interface for the SportDetails component.
+ */
 interface PropsType {
-  totalEvents:number;
-  sport:string
+  totalEvents: number; // The total number of events for the sport.
+  sport: string; // The name of the sport.
 }
 
-export default function SportDetails({sport,totalEvents}:PropsType) {
+/**
+ * SportDetails component represents details about a sport.
+ * @param totalEvents The total number of events for the sport.
+ * @param sport The name of the sport.
+ * @returns JSX element representing the SportDetails component.
+ */
+export default function SportDetails({ sport, totalEvents }: PropsType): React.JSX.Element {
+  // Render details about the sport
   return (
     <div className="bg-[#F7F7F8] dark:bg-[#292B32] flex p-2">
-        <div className="w-1/2">
-          <h3 className="text-[12px] text-[#525965] dark:text-[#DFDFDF]">Total Events</h3>
-          <h3 className="text-[14px] font-medium">
-            {totalEvents} Events
-          </h3>
-        </div>
-        <div>
-          <h3 className="text-[12px] text-[#525965] dark:text-[#DFDFDF]">Sport</h3>
-          <h3 className="text-[14px] font-medium">{sport}</h3>
-        </div>
+      {/* Render total number of events */}
+      <div className="w-1/2">
+        <h3 className="text-[12px] text-[#525965] dark:text-[#DFDFDF]">Total Events</h3>
+        <h3 className="text-[14px] font-medium">
+          {totalEvents} Events
+        </h3>
       </div>
-  )
+      {/* Render the sport name */}
+      <div>
+        <h3 className="text-[12px] text-[#525965] dark:text-[#DFDFDF]">Sport</h3>
+        <h3 className="text-[14px] font-medium">{sport}</h3>
+      </div>
+    </div>
+  );
 }
