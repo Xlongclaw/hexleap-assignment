51b6b259d7edf49c9911fe5b9a96c71b50221d8c
unknown <xlongclaw@gmail.com>
2024-04-01 14:52:42 +0530

    chore: remove unnecessary code

--- src/containers/navigation-bar/index.tsx
+++ src/containers/navigation-bar/index.tsx
@@ -1,22 +1,34 @@
-import Switch, { switchPositionType } from '@/components/switch'
-import { ThemeContext } from '@/themes/ThemeProvider'
-import React from 'react'
-import { BiSun } from 'react-icons/bi'
-import { BsMoonStarsFill } from 'react-icons/bs'
+import Switch, { switchPositionType } from "@/components/switch"; 
+import { ThemeContext } from "@/themes/ThemeProvider"; 
+import React from "react";
+import { BiSun } from "react-icons/bi";
+import { BsMoonStarsFill } from "react-icons/bs";
 
-const NavigationBar = () => {
+/**
+ * NavigationBar component represents the navigation bar with a theme switcher.
+ * @returns JSX element representing the NavigationBar component.
+ */
+const NavigationBar = (): JSX.Element => {
+  // Accessing the current theme from the ThemeContext
   const theme = React.useContext(ThemeContext);
 
-  const handleSwitchPress = (position:switchPositionType) =>{
-    if(position === 'left') theme?.changeTheme('dark')
-    else theme?.changeTheme('light')
-  }
-  return (
-    <nav className=' px-4 md:px-12 lg:px-32 py-8 flex justify-between items-center fixed w-full z-20'>
-      <h4 className='text-sm'></h4>
-      <Switch firstIcon={<BiSun/>} secondIcon={<BsMoonStarsFill/>} onPress={handleSwitchPress}/>
-    </nav>
-  )
-}
+  // Function to handle switch press and change theme
+  const handleSwitchPress = (position: switchPositionType) => {
+    if (position === "left") theme?.changeTheme("dark");
+    else theme?.changeTheme("light");
+  };
 
-export default NavigationBar
+  // Render the navigation bar with a theme switcher
+  return (
+    <nav className="px-4 md:px-12 lg:px-32 py-8 flex justify-end items-center fixed w-full z-20">
+      {/* Render the Switch component with icons and onPress handler */}
+      <Switch
+        firstIcon={<BiSun />}
+        secondIcon={<BsMoonStarsFill />}
+        onPress={handleSwitchPress}
+      />
+    </nav>
+  );
+};
+
+export default NavigationBar;
