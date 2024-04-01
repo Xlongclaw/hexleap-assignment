7db80460cf48fac23972fddd4921b0083045c266
unknown <xlongclaw@gmail.com>
2024-04-01 14:40:45 +0530

    refactor(docs): add comments and clean up unnecessary code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
-"use client";
+'use client'
 import ComponentShowcase from "@/containers/component-showcase";
-import { ThemeContext } from "@/themes/ThemeProvider";
+import { ThemeContext } from "@/themes/ThemeProvider"; 
 import React from "react";
 
-export default function Page() {
+/**
+ * Page component renders a ComponentShowcase within a themed environment.
+ * @returns JSX element representing the Page component.
+ */
+export default function Page(): React.JSX.Element {
+  // Accessing the current theme from the ThemeContext
   const theme = React.useContext(ThemeContext);
+
+  // Render the main component with the theme applied
   return (
     <main className={theme?.value}>
+      {/* Render the ComponentShowcase component */}
       <ComponentShowcase />
     </main>
   );
