9a5fc766bb7db7f6e6c5cf2668c5a08aeaf72870
unknown <xlongclaw@gmail.com>
2024-04-01 17:24:13 +0530

    refactor: add comments

--- src/containers/collection-spotlight/index.tsx
+++ src/containers/collection-spotlight/index.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import Heading from './heading'
-import { HEADING, SUBHEADING } from './constant'
-import CollectionContainer from './collection-container'
+import React from 'react'; 
+import Heading from './heading'; 
+import { HEADING, SUBHEADING } from './constant'; 
+import CollectionContainer from './collection-container'; 
 
-export default function CollectionSpotlight() {
+/**
+ * CollectionSpotlight component displays a collection spotlight.
+ * @returns JSX.Element
+ */
+const CollectionSpotlight: React.FC = () => {
   return (
     <div className='bg-gradient-to-b from-[#F9F8FF] to-[#F3F9FF] dark:from-[#18282A] dark:to-[#221A2C] w-full xl:w-[1200px] py-16'>
-      <Heading title={HEADING} subtitle={SUBHEADING} />
-      <CollectionContainer/>
+      <Heading title={HEADING} subtitle={SUBHEADING} /> {/* Render Heading component */}
+      <CollectionContainer /> {/* Render CollectionContainer component */}
     </div>
-  )
-}
+  );
+};
+
+export default CollectionSpotlight;
