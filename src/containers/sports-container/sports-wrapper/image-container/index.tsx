8e1995728f358275944492e43e11fdc0cd54df0f
unknown <xlongclaw@gmail.com>
2024-04-01 15:19:07 +0530

    refactor: add comments and clean up

--- src/containers/sports-container/sports-wrapper/image-container/index.tsx
+++ src/containers/sports-container/sports-wrapper/image-container/index.tsx
@@ -1,15 +1,28 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from 'next/image'; 
+import React from 'react';
 
+/**
+ * Props interface for the ImageContainer component.
+ */
 interface PropsType {
-  image:any
+  image: any; // The image source.
 }
 
-export default function ImageContainer({image}:PropsType) {
+/**
+ * ImageContainer component represents a container for an image.
+ * @param image The image source.
+ * @returns JSX element representing the ImageContainer component.
+ */
+export default function ImageContainer({ image }: PropsType): React.JSX.Element {
+  // Render the image container with the provided image source
   return (
     <div className="sm:h-96 xs:h-72 h-52 w-full overflow-hidden">
-      <Image src={image} alt="sport" 
-      className="w-full h-full object-cover object-top sm:object-center " />
-      </div>
-  )
+      {/* Render the image using the Next.js Image component */}
+      <Image
+        src={image} 
+        alt="sport" 
+        className="w-full h-full object-cover object-top sm:object-center" 
+      />
+    </div>
+  );
 }
