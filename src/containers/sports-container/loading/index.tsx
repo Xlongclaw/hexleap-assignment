d159ca4081d5feeda0d9191976308345f39dbf6b
unknown <xlongclaw@gmail.com>
2024-04-01 15:20:41 +0530

    refactor: add comments and clean up

--- src/containers/sports-container/loading/index.tsx
+++ src/containers/sports-container/loading/index.tsx
@@ -1,9 +1,16 @@
-import React from "react";
-import {Bars} from 'react-loader-spinner'
-export default function Loading() {
+import React from 'react';
+import { Bars } from 'react-loader-spinner';
+
+/**
+ * Loading component represents a loading indicator.
+ * @returns JSX element representing the Loading component.
+ */
+export default function Loading(): React.JSX.Element {
+  // Render the loading indicator centered vertically and horizontally
   return (
     <div className="h-[40rem] flex items-center justify-center">
-      <Bars height={32} color="#738FFF"/>
+      {/* Render the Bars loading spinner */}
+      <Bars height={32} color="#738FFF" />
     </div>
   );
 }
